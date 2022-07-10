7b2d8ff32d31229040830d20bd81c9661cb20ecc
Gabriel <grossiwm@gmail.com>
2022-07-10 17:31:47 -0300

    refactor

--- src/main/java/com/enderecoapp/EnderecoappApplication.java
+++ src/main/java/com/enderecoapp/EnderecoappApplication.java
@@ -3,8 +3,6 @@ package com.enderecoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableFeignClients
 @SpringBootApplication
--- src/main/java/com/enderecoapp/security/CustomAuthProvider.java
+++ src/main/java/com/enderecoapp/security/CustomAuthProvider.java
@@ -1,19 +1,15 @@
 package com.enderecoapp.security;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.enderecoapp.model.Usuario;
 import com.enderecoapp.repository.UsuarioRepository;
 
 @Service
